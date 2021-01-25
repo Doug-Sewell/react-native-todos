3c4587cf09e860d749fc3b4f2a2912952b7e1931
barrettbolivar <barrettbolivar@gmail.com>
2021-01-25 15:28:16 -0500

    Added multi-use validator

--- main/SplashPage/SplashPage.tsx
+++ main/SplashPage/SplashPage.tsx
@@ -1,25 +1,59 @@
 import React, { useState } from 'react';
 import { Text, TextInput, Button } from 'react-native';
+import Validator from '../utils/validator';
 
 const SplashPage = () => {
 	const [userId, setUserId] = useState('');
 	const [password, setPassword] = useState('');
+	const userValidator = new Validator('', 0, '');
+
+	const onInput = (input: string, inputType: string) => {
+		if (userValidator.getAttempts() >= 3) {
+			// no more than three attempts per hour maybe idk
+			// set error state
+			return;
+		}
+		switch(inputType) {
+			case 'UserId':
+				// UserId validation on backend, check for unwanted regex characters, or script tags. Make a verification reusable class
+				userValidator.setItem(inputType);
+				userValidator.setValidatee(input);
+				if (userValidator.isSafe()) {
+					// safe to submit
+				} else {
+					// not safe
+				}
+			case 'PW':
+				// Password validation here, check for unwanted characters
+				userValidator.setItem(inputType);
+				userValidator.setValidatee(input);
+				if (userValidator.isSafe()) {
+					// safe to submit
+				} else {
+					// not safe
+				}
+		}
+		userValidator.setAttempts(userValidator.getAttempts() + 1);
+	}
+
 	const onPress = () => {
+		// Api call to backend to verify things as necessary
 		return;
 	}
+
 	return (
 		<div>
-			<Text>Hello, I am your SplashPage!</Text>
+			<Text>Hello, I am your SplashPage! Input your details to login.</Text>
 			<TextInput
 				style={{height: 40, display: 'flex'}}
 				placeholder="Enter user id"
-				onChangeText={text => setUserId(text)}
+				onChangeText={text => onInput(text, 'UserId')}
 				defaultValue={ userId }
 			/>
 			<TextInput
 				style={{height: 40, display: 'flex'}}
 				placeholder="Enter password"
-				onChangeText={text => setPassword(text)}
+				onChangeText={text => onInput(text, 'PW')}
 				defaultValue={ password }
 			/>
 			<Button
